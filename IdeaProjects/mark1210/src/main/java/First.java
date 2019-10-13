078d2f1625503a9965eebaba1eeec94a9faad024
herculmrk <kaloukh7@gmail.com>
2019-10-13 20:40:38 +0200

    My mess

---
+++ IdeaProjects/mark1210/src/main/java/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hi Mark");
+        for (int i=0;i<10;i++)
+            System.out.println(Math.cbrt(i));
+
+    }
+}
