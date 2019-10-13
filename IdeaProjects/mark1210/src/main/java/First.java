6c670abc0ba7ce1debb255983f32ccfd31e27810
herculmrk <kaloukh7@gmail.com>
2019-10-13 21:32:44 +0200

    My tesing

--- IdeaProjects/mark1210/src/main/java/First.java
+++ IdeaProjects/mark1210/src/main/java/First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hi Mark");
+        System.out.println("Hi Markkkkkkkkkkkki");
         for (int i=0;i<10;i++)
             System.out.println(Math.cbrt(i));
 
